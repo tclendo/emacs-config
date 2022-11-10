f5b0f182893b663dc879d1e240a896460e96435d
Troy Clendenen <troy.clendenen@digital.ai>
2022-11-10 15:00:17 -0500

    update emacs config

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("b0334e8e314ea69f745eabbb5c1817a173f5e9715493d63b592a8dc9c19a4de6" default))
+ '(helm-minibuffer-history-key "M-p")
+ '(org-agenda-files '("/Users/troyclendenen/todo.org"))
+ '(package-selected-packages
+   '(doom-themes flatland beacon highlight-indent-guides ample highlight-indent-guide helm focus restart-emacs dimmer smartparens sublimity yasnippet which-key use-package undo-tree sml-mode rust-mode powerline magit lsp-ui ivy gruvbox-theme flycheck evil eglot company))
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
